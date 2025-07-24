b8e6615ff3f568cdca61287cc3d853d1329c239a
Serghei Filimonov <filimonov.sergey@gmail.com>
2025-07-24 15:43:20 +0300

    Add RDS module.

--- main.tf
+++ main.tf
@@ -107,3 +107,39 @@ module "argo_cd" {
   namespace    = "argocd"
   chart_version = "5.46.4"
 }
+
+# Підключаємо модуль RDS
+module "rds" {
+  source = "./modules/rds"
+
+  name                       = "django-app-db"
+  use_aurora                 = false
+  aurora_instance_count      = 2
+
+  # --- RDS-only ---
+  engine                     = "postgres"
+  engine_version             = "17.2"
+  parameter_group_family_rds = "postgres17"
+
+  # Common
+  instance_class             = "db.t3.medium"
+  allocated_storage          = 20
+  db_name                    = "myapp"
+  username                   = "postgres"
+  password                   = "admin123AWS23"
+  subnet_private_ids         = module.vpc.private_subnets
+  subnet_public_ids          = module.vpc.public_subnets
+  publicly_accessible        = false
+  vpc_id                     = module.vpc.vpc_id
+  multi_az                   = true
+  backup_retention_period    = 7
+  parameters = {
+    max_connections              = "200"
+    log_min_duration_statement   = "500"
+  }
+
+  tags = {
+    Environment = "dev"
+    Project     = "myapp"
+  }
+} 
